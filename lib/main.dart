6ee071e2dd834b6ac51add6a26b15b00d3733495
lfenix_dart_flutter <lfenix_dart_flutter@lfenix-youtubes-Mac-mini.local>
2023-12-25 07:48:40 -0600

    corrected question

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,16 @@
 /*
-Practice Question 1: Building a Dart Function Utilizing Various Concepts
+Practice Question 2: Dart Class with Null Safety
 
 Task Description:
-Create a Dart function named analyzeText that takes a string and returns a Map with three keys: length (the length of the string), hasDigits (a boolean indicating if the string contains any digits), and reversed (the string in reverse order). Implement this function by utilizing Dart's string manipulation and collection features.
+Create a Dart class named UserProfile with properties name (String), age (nullable int), and favoriteColor (String with a default value of 'blue'). Implement a method profileSummary that returns a string summarizing the user's profile. Ensure null safety is appropriately handled.
  */
 
-import 'package:characters/characters.dart';
+class UserProfile {
+  String name;
+  int? age;
+  String favoriteColor;
 
-Map<String, dynamic> analyzeText(String input) {
-  final ans = <String,
-      dynamic>{}; //declaring a map this way is also possible; don't need the constructor
+  UserProfile({required this.name, this.age, this.favoriteColor = 'blue'});
 
-  ans['length'] = input.length;
-  ans['hasDigits'] = input.digits();
-  ans['reversed'] = input.characters.toList().reversed.join();
-
-  return ans;
-}
-
-extension HasDigits on String {
-  bool digits() {
-    for (var indivChar in characters) {
-      if (RegExp(r'[0-9]').hasMatch(indivChar)) {
-        return true;
-      }
-    }
-    return false;
-  }
+  String profileSummary() => 'Name: $name, Age: ${age ?? 'not specified'}, Favorite Color: $favoriteColor' ;
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,16 +2,16 @@ import 'package:dartalanguageyoualreadyknow_dartclasswithnullsafety/main.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('analyzeText function test', () {
-    var result = analyzeText('Hello123');
+  test('UserProfile class test', () {
+    UserProfile user1 = UserProfile(name: 'Alice', age: 30);
+    UserProfile user2 = UserProfile(name: 'Bob'); // No age provided
 
-    // Test for length
-    expect(result['length'], 8);
+    // Test user1 profile summary
+    expect(
+        user1.profileSummary(), 'Name: Alice, Age: 30, Favorite Color: blue');
 
-    // Test for hasDigits
-    expect(result['hasDigits'], true);
-
-    // Test for reversed
-    expect(result['reversed'], '321olleH');
+    // Test user2 profile summary with null age
+    expect(user2.profileSummary(),
+        'Name: Bob, Age: not specified, Favorite Color: blue');
   });
 }
